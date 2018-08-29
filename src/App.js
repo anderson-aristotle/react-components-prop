fadbe1a32eb06e93c8cc53b74d77f95d2869e1dc
jordanallain <jordan.allain@generalassemb.ly>
2018-08-29 11:29:05 -0400

    Edit App component starter code
    - Edit the starter code for the App component to reflect the previous
    lesson.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.scss'
 
 import Movie from './Movie.js'
 
-const movies = ['Dr. Strangelove', 'Eraserhead', 'Fantastic Mr. Fox']
+const movies = [{title: 'Dr. Strangelove'}, {title: 'Eraserhead'}, {title: 'Fantastic Mr. Fox'}]
 
 const App = () => (
   <div>
     <h1>Welcome to React!</h1>
-    {movies.map(title => (
-      <Movie key={title} title={title} />
+    {movies.map(movie => (
+      <Movie key={movie.title} title={movie.title}/>
     ))}
   </div>
 )
