e10e1655681bf78b448b79c44909814b49521c15
jordanallain <jordan.allain@generalassemb.ly>
2018-11-06 12:01:40 -0500

    Add solution code
    - Refresh solution for cohort 027.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,75 @@ import './App.scss'
 
 import Movie from './Movie.js'
 
-const movies = [{title: 'Dr. Strangelove'}, {title: 'Eraserhead'}, {title: 'Fantastic Mr. Fox'}]
+const movies = [
+  {
+    id: 1,
+    title: 'Dr. Strangelove',
+    director: 'Stanley Kubrick',
+    cast: [
+      {
+        name: 'Peter Sellers',
+        role: 'President Merkin Muffley'
+      },
+      {
+        name: 'George C. Scott',
+        role: 'General Buck Turgidson'
+      },
+      {
+        name: 'Slim Pickens',
+        role: 'Major T.J. "King" Kong'
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: 'Eraserhead',
+    director: 'David Lynch',
+    cast: [
+      {
+        name: 'Jack Nance',
+        role: 'Henry Spencer'
+      },
+      {
+        name: 'Charlotte Stewart',
+        role: 'Mary X'
+      },
+      {
+        name: 'Jeanne Bates',
+        role: 'Mrs. X'
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: 'Fantastic Mr. Fox',
+    director: 'Wes Anderson',
+    cast: [
+      {
+        name: 'George Clooney',
+        role: 'Mr. Fox'
+      },
+      {
+        name: 'Meryl Streep',
+        role: 'Mrs. Fox'
+      },
+      {
+        name: 'Bill Murray',
+        role: 'Badger'
+      }
+    ]
+  }
+]
 
 const App = () => (
   <div>
     <h1>Welcome to React!</h1>
     {movies.map(movie => (
-      <Movie key={movie.title} title={movie.title}/>
+      <Movie
+        key={movie.id}
+        title={movie.title}
+        director={movie.director}
+        cast={movie.cast}/>
     ))}
   </div>
 )
--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
+import Actor from './Actor'
 
 class Movie extends Component {
   render () {
     return (
-      <h4>Title: {this.props.title}</h4>
+      <div>
+        <h4>Title: {this.props.title}</h4>
+        <p>Director: {this.props.director}</p>
+        <p>Starring:</p>
+        <ul>
+          {this.props.cast.map(actor => (
+            <Actor
+              key={actor.name}
+              name={actor.name}
+              role={actor.role}
+            />
+          ))}
+        </ul>
+      </div>
     )
   }
 }
